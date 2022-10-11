73f9378b92f44447214ed16009ed0435b4c98a10
CodesManiac <joyajiboye2016@gmail.com>
2022-10-11 03:13:21 +0100

    fetched users and posts data and display users data

--- components/User.tsx
+++ components/User.tsx
@@ -1,7 +1,7 @@
 import { Message } from 'iconsax-react';
 import React from 'react';
 
-const User = () => {
+const User = ({ user }: any) => {
   return (
     <div className='bg-secondary p-6 rounded-2xl space-y-4'>
       <div className='flex space-x-3'>
@@ -12,9 +12,9 @@ const User = () => {
         />
         <div className='flex w-4/5 justify-between'>
           <div className='spac-y-2'>
-            <p className=' font-semibold text-xl'>John Doe</p>
-            <p className='font-medium text-lg'> Jenny</p>
-            <p className='font-medium text-lg'>jay@gmail.com</p>
+            <p className=' font-semibold text-xl'>{user.name}</p>
+            <p className='font-medium text-lg'>{user.username}</p>
+            <p className='font-medium text-lg'>{user.email}</p>
           </div>
           <p className='flex space-x-2 items-center cursor-pointer'>
             <Message size='16' color='#0f172a' />{' '}
